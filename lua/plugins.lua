4d25308d405049160bb1249e39b52f4005d1ac69
yangxian <yangxian@miliantech.com>
2025-04-02 18:23:03 +0800

    go vim dep

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -280,14 +280,9 @@ require("lazy").setup({
     end,
     opts = {
       blame_options = { '-w' },
-    },
-    {
+  },
+  {
       "ray-x/go.nvim",
-      dependencies = {  -- optional packages
-        "ray-x/guihua.lua",
-        "neovim/nvim-lspconfig",
-        "nvim-treesitter/nvim-treesitter",
-      },
       config = function()
         require("go").setup()
       end,
